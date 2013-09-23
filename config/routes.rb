0fd5e0b3122996cafdab1125b35bef7a50d0d3e3
Peng Zhong <nylira@riseup.net>
2013-09-23 20:46:47 +0800

    basic I18n and Pages#index homepage

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ PrismBreak::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
