4fce2c0db100a1bc0e3c19bc7bf00b79bf771cd9
Peng Zhong <nylira@riseup.net>
2013-10-03 12:55:26 +0800

    rename 'locales' to 'accepted_locales' to prevent possible conflicts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-  def locales
+  def accepted_locales
     I18n.available_locales.sort
   end
 
   def languages
-    locales.map do |locale|
+    accepted_locales.map do |locale|
       [I18n.t('i18n.language.name', locale: locale), locale.to_s]
     end
   end
