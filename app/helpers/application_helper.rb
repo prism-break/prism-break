d8662875f6f290b3789662d4c48713800efb0591
Peng Zhong <nylira@riseup.net>
2013-10-03 12:53:19 +0800

    implement working 'language_list' and 'language_select' partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,10 @@
 module ApplicationHelper
 
-  def languages
-    [
-      ['English', 'en'],
-      ['Deutsch', 'de'],
-      ['Nihongo', 'ja']
-    ]
-  end
-
   def locales
     I18n.available_locales.sort
   end
 
-  def locale_name_pairs
+  def languages
     locales.map do |locale|
       [I18n.t('i18n.language.name', locale: locale), locale.to_s]
     end
