fecff7b3713a59505be69034b5ca5c951beb5d71
Peng Zhong <nylira@riseup.net>
2013-10-05 22:04:32 +0800

    add validations to software.rb

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,4 +1,11 @@
 class Software < ActiveRecord::Base
+  # validations
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :url, presence: true
+  validates :source_url, presence: true
+
+  # translations and edit history
   translates :title, :description, :url, :source_url, :privacy_url, :tos_url, :versioning => true
   has_paper_trail
 end
