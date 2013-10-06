0f228805ad1b9c65dc0ec299c364b4a42d11ba05
Peng Zhong <nylira@riseup.net>
2013-10-06 21:19:44 +0800

    make it easier to edit categories

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module PrismBreak
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
   end
 end
