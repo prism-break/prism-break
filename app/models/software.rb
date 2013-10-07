ebe2e8851b703893ae7759043ccff9da104bd337
Peng Zhong <nylira@riseup.net>
2013-10-07 20:29:59 +0800

    add URL validations to software attributes

--- app/models/software.rb
+++ app/models/software.rb
@@ -5,6 +5,11 @@ class Software < ActiveRecord::Base
   validates :url, presence: true
   validates :source_url, presence: true
 
+  validates_format_of :url, :source_url, :privacy_url, :tos_url,
+    :with => URI::regexp(%w(http https)),
+    :message => "requires 'https://' or 'http://'",
+    :allow_blank => :true
+
   # translations and edit history
   translates :title, :description, :url, :source_url, :privacy_url, :tos_url, :versioning => true
   has_paper_trail
