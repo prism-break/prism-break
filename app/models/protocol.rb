4e984526ed6b4f0537ac326c176cc8eba7fe915b
Peng Zhong <nylira@riseup.net>
2013-10-15 00:06:53 +0800

    update Protocol model with validations

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,4 +1,10 @@
 class Protocol < ActiveRecord::Base
+  # localization
   translates :title, :description, :url, :versioning => true
   has_paper_trail
+
+  # validations
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true
 end
