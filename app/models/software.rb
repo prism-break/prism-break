79dc80e2abfb7463c2db78e88d1d2712d98bc749
Peng Zhong <nylira@riseup.net>
2013-10-24 17:18:12 +0800

    rm Mumble logo and remove 2x references in software.rb

--- app/models/software.rb
+++ app/models/software.rb
@@ -16,22 +16,13 @@ class Software < ActiveRecord::Base
   # paperclip
   has_attached_file :logo,
     :styles => {
-      :large_2x => "240x240",
       :large => "120x120",
-      :medium_2x => "120x120",
       :medium => "60x60",
-      :legacy_2x => "100x100>",
       :legacy => "50x50>",
-      :small_2x => "80x80>",
       :small => "40x40>",
-      :tiny_2x => "48x48>",
       :tiny => "24x24>",
     },
-    :default_style => :legacy,
     :default_url => "/images/:style/missing.png",
-    :convert_options => {
-      :all => "-strip -alpha set"
-    }
 
   # validations
   validates :title, presence: true, uniqueness: true
@@ -52,17 +43,9 @@ class Software < ActiveRecord::Base
   validates_attachment_content_type :logo,
     :content_type => /^image\/(png|x-png|svg|svg+xml)$/,
     :message => 'should only be png or svg'
-  validate :attachment_is_square, :unless => "errors.any?"
 
   # attachment deletion
   attr_accessor :delete_logo
   before_validation { logo.clear if delete_logo == '1' }
-
-  def attachment_is_square
-    dimensions = Paperclip::Geometry.from_file(logo.queued_for_write[:original].path)
-    if dimensions.width != dimensions.height
-      errors.add(:logo,'width and height should be equal')
-    end
-  end
 
 end
