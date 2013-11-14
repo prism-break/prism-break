6bbfc9a444669b73d6f63fe8822d4edd5be5870a
Peng Zhong <nylira@riseup.net>
2013-11-14 15:11:06 +0800

    update some views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -38,7 +38,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to @category.platform, notice: 'Category was successfully created.' }
         format.json { render action: 'show', status: :created, location: @category }
       else
         format.html { render action: 'new' }
