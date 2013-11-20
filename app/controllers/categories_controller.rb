fbb33439f8524eaed588899c41431a5dd4e058c1
Peng Zhong <nylira@riseup.net>
2013-11-20 09:47:11 +0800

    add social networks and disconnect search

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -66,7 +66,7 @@ class CategoriesController < ApplicationController
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url }
+      format.html { redirect_to root_url }
       format.json { head :no_content }
     end
   end
